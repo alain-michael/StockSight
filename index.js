479716940b73409f494e95d5826f7eda847b4329
alain-michael <154985928+alain-michael@users.noreply.github.com>
2024-03-04 13:23:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const startDate = document.querySelector('[name="start-date"]');
 const endDate = document.querySelector('[name="end-date"]');
 const switcher = document.querySelector("#choose-mode");
 const body = document.querySelector("body");
+const loadingSpinner = document.getElementById("loadingSpinner");
+
+function showLoadingSpinner() {
+  loadingSpinner.style.display = "block";
+}
+
+function hideLoadingSpinner() {
+  loadingSpinner.style.display = "none";
+}
 
 function processData(data) {
+    // Function to process data and create a visualization of it.
     // Set up SVG dimensions
     const margin = { top: 20, right: 20, bottom: 30, left: 40 };
     const width = 1200 - margin.left - margin.right;
@@ -66,37 +76,62 @@ function processData(data) {
             .style("left", (event.pageX + 20) + "px")
             .style("top", (event.pageY - 20) + "px");
     
-        // Make circle visible when hovered
+        // Make circle visible when hovered over
         d3.select(this).attr("fill", "steelblue").attr('fill-opacity', 1);
     })
     .on("mousemove", function(event) {
-        // Update tooltip position
+        // Update tooltip position to follow the mouse
         tooltip.style("left", (event.pageX + 20) + "px")
             .style("top", (event.pageY - 20) + "px");
     })
     .on("mouseout", function() {
         // Hide tooltip
-        tooltip.transition().duration(100).style("opacity", 0);
+        tooltip.transition().duration(400).style("opacity", 0);
     
         // Reset circle appearance
-        d3.select(this).attr("fill-opacity", 0);
+        d3.select(this).transition().duration(100).attr("fill-opacity", 0);
     });
     
     
 }
 
 function getStockData(stock_name, start_date, end_date) {
-    console.log(startDate.value, endDate.value)
-    headers = {"User-Agent": "MyAPI 0.0.1", "Authorization": "Bearer qgqm4upxttwKpNp2fZrq1j3dgHu7zJMn"}
-    fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2024-01-14?adjusted=true&sort=asc`, {
-        headers: headers,
-    }).then(response => response.json())
-    .then(data => processData(data.results))
+    // Function to scrape the data
+    console.log(startDate.value, endDate.value);
+    const headers = {
+        "User-Agent": "MyAPI 0.0.1",
+        "Authorization": "Bearer qgqm4upxttwKpNp2fZrq1j3dgHu7zJMn"
+    };
+    
+    return new Promise((resolve, reject) => {
+        fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2024-01-14?adjusted=true&sort=asc`, {
+            headers: headers,
+        })
+        .then(response => response.json())
+        .then(data => {
+            processData(data.results);
+            resolve(); // Resolve the Promise after data processing so that the rest can process
+        })
+        .catch(error => reject(error));
+    });
+    
 }
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    showLoadingSpinner();
     getStockData(stockName,startDate, endDate)
+    .then(() => {
+        hideLoadingSpinner(); // Hide loading spinner when data processing is complete
+        const svgElement = document.getElementById("stockChart");
+        const svgPosition = svgElement.getBoundingClientRect().top;
+  
+        // Scroll the page to the SVG element
+        window.scrollTo({
+          top: svgPosition,
+          behavior: "smooth"
+        });
+      });
 });
 
 switcher.addEventListener("click", () => {
